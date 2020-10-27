affcb32cc41fc7c91d90329918ceb6bc6275e82c
Audrey <audrey_cooper@me.com>
2020-10-27 15:48:49 -0700

    MaterialUI theme added

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,8 +3,11 @@ import { Route, Switch } from 'react-router-dom'
 import MyCalendar from './Calendar/Calendar'
 import Home from './Home/Home'
 
+
+
 const App = () => {
   return (
+
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/calendar" component={MyCalendar}/>
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -7,12 +7,59 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../components/App'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#004578',
+    },
+    secondary: {
+      main: '#a47638',
+    },
+      // Used by `getContrastText()` to maximize the contrast between
+      // the background and the text.
+      contrastThreshold: 3,
+      // Used by the functions below to shift a color's luminance by approximately
+      // two indexes within its tonal palette.
+      // E.g., shift from Red 500 to Red 300 or Red 700.
+      tonalOffset: 0.2,
+  },
+  typography: {
+    body1: {
+      fontSize: "1.5rem",
+    },
+    subtitle1: {
+      fontSize: "1.5rem",
+    },
+    h6: {
+      fontSize: "1.75rem",
+    },
+    h5: {
+      fontSize: "2rem",
+    },
+    h4: {
+      fontSize: "2.75rem"
+    },
+    h3: {
+      fontSize: "3.5rem"
+    },
+    h2: {
+      fontSize: "4.5rem"
+    },
+    button: {
+      fontSize: "1.5rem"
+    }
+  }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Router>
-      <Route path='/' component={App}/>
-    </Router>,
+    <ThemeProvider theme={theme}>
+      <Router>  
+        <Route path='/' component={App}/>
+      </Router>,
+    </ThemeProvider>,
     document.body.appendChild(document.createElement('div')),
   )
 })
