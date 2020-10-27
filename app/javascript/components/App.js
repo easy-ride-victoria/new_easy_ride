ca86aab6d7962ee81154342a53ca7672ce26eb1f
Iyris <iyrisvigil@gmail.com>
2020-10-26 18:58:59 -0700

    use react-router to render home and calendar

---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import MyCalendar from './Calendar/Calendar'
+import Home from './Home/Home'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/calendar" component={MyCalendar}/>
+    </Switch>
+  )
+}
+ 
+export default App;
