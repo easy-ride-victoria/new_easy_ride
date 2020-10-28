1c8adf4b9651cb87d08516d5491a644081590c82
Nicole Woodcock <nicole.xox@gmail.com>
2020-10-27 23:28:29 -0700

    add test and validation on rides & horses

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,3 +1,9 @@
 class Horse < ApplicationRecord
   has_many :rides
+  has_many :bookings, through: :rides
+
+  def has_ride_on_day?(day)
+    bookings.where("start_time::date = ?", day).length > 0
+  end
+
 end
