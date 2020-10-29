041be5c29250f00fee0198233379fe6f758bf83b
Iyris <iyrisvigil@gmail.com>
2020-10-28 20:02:43 -0700

    add outlined variants to textfields

--- app/javascript/components/Profile/ProfilePage.js
+++ app/javascript/components/Profile/ProfilePage.js
@@ -5,16 +5,28 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from "@material-ui/core/styles";
 import MenuAppBar from '../Layout/NavBar';
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-  }
+    flexWrap: 'wrap'
+  },
+  form: {
+    width: "100%",
+    marginLeft: theme.spacing(4),
+    marginRight: theme.spacing(4),
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(8),
+    width: '15ch',
+  },
 }));
 
 const ProfileRoute = (props) => {
@@ -45,25 +57,31 @@ const ProfileRoute = (props) => {
       <MenuAppBar currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <div className={classes.paper}>
         <form className={classes.form} >
-          <TextField id="standard-basic" label="First name" value={values.first_name} onChange={(e) => {
+          <Typography component="h3" variant="h3">
+            My Profile
+          </Typography>
+          <br></br><br></br>
+          <TextField variant="outlined" label="First name" value={values.first_name} onChange={(e) => {
             setValues({...values, first_name: e.target.value});
           }}/>
-          <br></br>
-          <TextField id="standard-basic" label="Last name" value={values.last_name} onChange={(e) => {
+          <br></br><br></br>
+          <TextField variant="outlined" label="Last name" value={values.last_name} onChange={(e) => {
             setValues({...values, last_name: e.target.value});
           }}/>
-          <br></br>
-          <TextField id="standard-basic" label="Email" value={values.email} onChange={(e) => {
+          <br></br><br></br>
+          <TextField variant="outlined" label="Email" value={values.email} onChange={(e) => {
             setValues({...values, email: e.target.value});
           }}/>
+          <br></br><br></br>
+          <TextField variant="outlined" label="Password" value="******"/>
+          <Link href="#" variant="subtitle1">
+            <FormHelperText id="my-helper-text">Forgot your password?</FormHelperText>
+          </Link>
           <br></br>
-          <TextField id="standard-basic" label="Password" value="******"/>
-          <FormHelperText id="my-helper-text">Forgot your password?</FormHelperText>
-       
-          <TextField id="standard-basic" label="HCBC Number" value={values.hcbc_number} onChange={(e) => {
+          <TextField variant="outlined" label="HCBC Number" value={values.hcbc_number} onChange={(e) => {
             setValues({...values, hcbc_number: e.target.value});
           }}/>
-          <br></br>
+          <br></br><br></br>
           <FormControlLabel
             control={
               <Checkbox
@@ -77,7 +95,7 @@ const ProfileRoute = (props) => {
             }
             label="Active HCBC"
           />
-
+          <br></br><br></br>
           <Button
             type="submit"
             variant="contained"
