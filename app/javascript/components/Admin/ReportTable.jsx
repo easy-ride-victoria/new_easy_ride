0afea99aa2561b4c0b0f43aa872985ab934e3cfd
Audrey <audrey_cooper@me.com>
2020-10-30 23:34:06 -0700

    Report table for admin added

--- app/javascript/components/Admin/ReportTable.jsx
+++ app/javascript/components/Admin/ReportTable.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 // import EditHorseForm from "./EditHorseForm";
 
 // Material ui customization
@@ -33,14 +34,23 @@ const useStyles = makeStyles({
   tableCellBody: {
     fontSize: "1.5rem",
     fontFamily: "Roboto",
-  }
+  },
+  avatar: {
+    backgroundColor: "#004578",
+    width: "35px",
+    height:"35px"
+  },
 });
+const style = {
+  width: "25px",
+  height: "25px"
+}
 
 export default function ReportsTable(props) {
   const classes = useStyles();
-  const { reports, onChange } = props;
+  const reports = props;
 
-  // console.log("horses: ", horses);
+  console.log("reports: ", reports);
   return (
     <TableContainer >
       <Table className={classes.table} aria-label="customized table">
@@ -54,12 +64,14 @@ export default function ReportsTable(props) {
             <TableCell className={classes.tableCellHead} >Activity</TableCell>
             <TableCell className={classes.tableCellHead} >Exercises</TableCell>
             <TableCell className={classes.tableCellHead} >Comments</TableCell>
+            <TableCell className={classes.tableCellHead} >Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {reports.map((report) => {
   //  change from here
             const {
+              id,
               activity_date,
               answer1,
               answer2,
@@ -83,12 +95,11 @@ export default function ReportsTable(props) {
                 <TableCell className={classes.tableCellBody} >{answer3}</TableCell>
                 
                 <TableCell className={classes.tableCellBody} >{answer4}</TableCell>
-                {/* <TableCell align="right"> */}
-                  {/* <EditHorseForm
-                    horse={horse}
-                    onSubmit={onChange}
-                  ></EditHorseForm> */}
-                {/* </TableCell> */}
+                <TableCell className={classes.tableCellBody} >
+                <Avatar className={classes.avatar}>
+                  <DeleteOutlineIcon style={style} onClick={() => Axios.delete(`api/v1/reports/${id}`)}/>
+                  </Avatar>
+                  </TableCell>
               </TableRow>
             );
           })}
