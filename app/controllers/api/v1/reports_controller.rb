ce7f29c476d62c1f3bfa349e6e177f3460a54c0e
Audrey <audrey_cooper@me.com>
2020-11-01 11:42:45 -0800

    reports_controller tested with insomnia

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -14,10 +14,12 @@ module Api
       end
 
       def create
-        user = User.find_by(email: params[:email])
-        horse = Horse.find_by(name: params[:horse])
+        user = User.find_by("last_name = ? AND first_name = ?", params[:last_name], params[:first_name])
+        puts user
+        horse = Horse.find_by("name = ?", params[:horse])
+        puts horse
        
-        report = Report.new({user_id: params[:user_id], horse_id: params[:horse_id], activity_date: params[:activity_date], answer1: params[:answer1], answer2: params[:answer2],answer3: params[:answer3], answer4: params[:answer4]})
+        report = Report.new({user: user, horse: horse, activity_date: params[:activity_date], answer1: params[:answer1], answer2: params[:answer2],answer3: params[:answer3], answer4: params[:answer4]})
         if report.save
           render json: ReportSerializer.new(report).serializable_hash.to_json
         else
