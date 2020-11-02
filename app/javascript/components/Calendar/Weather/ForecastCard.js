7ce72950cf7c4029f8205061618e4ddf2af7dadc
Iyris <iyrisvigil@gmail.com>
2020-11-02 13:14:01 -0800

    delete with styles

--- app/javascript/components/Calendar/Weather/ForecastCard.js
+++ app/javascript/components/Calendar/Weather/ForecastCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
