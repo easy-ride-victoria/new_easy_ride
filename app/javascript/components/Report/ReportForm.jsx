85934d60ea7cfe26e0e7440064d8cf5d6d1fafa0
Audrey <audrey_cooper@me.com>
2020-11-16 18:39:53 -0800

    form working. API post missing

--- app/javascript/components/Report/ReportForm.jsx
+++ app/javascript/components/Report/ReportForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Grid, TextField, InputLabel,  FormControlLabel, Typography, Checkbox, FormControl, Select, MenuItem } from '@material-ui/core';
+import { Button, Grid, TextField, InputLabel,  FormControlLabel, Typography, Checkbox, FormControl, Select, MenuItem, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText } from '@material-ui/core';
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Axios from "axios";
 import { SettingsPowerRounded } from "@material-ui/icons";
@@ -17,7 +17,8 @@ const useStyles = makeStyles((theme) => ({
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
+    minWidth: 250,
+    maxWidth:500,
   },
 }));
 
@@ -28,7 +29,7 @@ const initialState = {
   horse: "", 
   activity_date: "", 
   answer1: true, 
-  answer2: "10", 
+  answer2: "", 
   answer3: "", 
   answer4: ""
 }
@@ -39,7 +40,8 @@ export default function AddReportForm(props) {
   const classes = useStyles();
   
   const stylesTextArea = {
-    width: "500px" 
+    minWidth: "300px",
+    maxWidth:"55px"
   }
 
   const stylesTitle = {
@@ -57,8 +59,8 @@ export default function AddReportForm(props) {
     event.preventDefault();
     // Axios.post("/api/v1/reports", values)
     //     .then( () => {
-    //       console.log ("sent!")
-        setOpen(true);
+          console.log ("sent!")
+       
       // })
   }
 
@@ -80,109 +82,150 @@ export default function AddReportForm(props) {
     setOpen(true);
     };
   
-  console.log ("state:", state.answer2)
+  console.log ("state:", state)
   
   // rendering
   return (
-    
-    <Grid container 
-    alignItems="stretch" justify="space-around" direction="column" >
+    <div>
+      <Grid container 
+      alignItems="stretch" justify="space-around" direction="column" >
 
-      <Grid item xs className={classes.title}>
-        <Typography gutterBottom variant="h4">
-          Please fill your report here.
-        </Typography>
-      </Grid>
-      
-      <form onSubmit={handleSubmit}>
-
-        <Grid item xs className={classes.textfield}>
-          <TextField id="filled-multiline-static" label="First Name" variant="outlined" name="first_name" style={stylesTextArea} type="text" rows={5}
-          InputLabelProps={{
-            shrink: true,
-          }}/>
-          
+        <Grid item xs className={classes.title}>
+          <Typography gutterBottom variant="h4">
+            Please fill your report here.
+          </Typography>
         </Grid>
-      
+        
+        <form onSubmit={handleSubmit}>
 
-        <Grid item xs className={classes.textfield}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" name="last_name" style={stylesTextArea} type="text" label="Last Name" InputLabelProps={{shrink: true,}} />
-         
-        </Grid>
+          <Grid item xs className={classes.textfield}>
+            <TextField id="filled-multiline-static" label="First Name" variant="outlined" name="first_name" 
+            onChange={handleChange}
+            value={state.first_name}
+            style={stylesTextArea} 
+            type="text"
+            InputLabelProps={{
+              shrink: true,
+            }}/>
+            
+          </Grid>
+        
 
-        <Grid item xs className={classes.textfield}>
-          <TextField id="filled-multiline-static" variant="outlined" name="horse" style={stylesTextArea} type="text" label="Horse Name" rows={5} InputLabelProps={{shrink: true,}} />
-       
-        </Grid>
+          <Grid item xs className={classes.textfield}>
+            <TextField 
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+              name="last_name" 
+              style={stylesTextArea} 
+              type="text" 
+              label="Last Name"
+              onChange={handleChange}
+              value={state.last_name} 
+              InputLabelProps={{shrink: true,}} />
+          </Grid>
 
-        <Grid item xs className={classes.textfield}>
-          <TextField id="filled-multiline-static" variant="outlined" name="activity_date" style={stylesTextArea} type="text" label="Date of your ride or other activity" rows={5} InputLabelProps={{shrink: true,}} />
-      
-        </Grid>
+          <Grid item xs className={classes.textfield}>
+            <TextField 
+              id="filled-multiline-static"
+              variant="outlined" 
+              name="horse" 
+              style={stylesTextArea} 
+              type="text" 
+              label="Horse Name"
+              onChange={handleChange}
+              value={state.horse}
+              InputLabelProps={{shrink: true,}} 
+            />        
+          </Grid>
 
-      <Grid item xs className={classes.textfield}>
-        <FormControlLabel 
-          control = {
-            <Checkbox
-              checked={state.answer1}
-              onChange={handleChecked}
-              name="answer1"
-              color="secondary" 
+          <Grid item xs className={classes.textfield}>
+            <TextField 
+              id="filled-multiline-static"
+              variant="outlined" 
+              name="activity_date" 
+              style={stylesTextArea} 
+              type="text" 
+              label="Date of your ride or other activity" 
+              onChange={handleChange}
+              value={state.activity_date}
+              InputLabelProps={{shrink: true,}} 
             />
-          }
-          label="Did you warm-up with a 10 minutes walk on a loose rein?"
-        />
-      </Grid>
+          </Grid>
 
-      <Grid item xs className={classes.textfield}>
-      <Button className={classes.button} onClick={handleOpen}>
-        Open the select
-      </Button>
-        <FormControl>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={state.answer2}
-          onChange={handleSelect}
-        >
-          <MenuItem value=""> </MenuItem>
-          <MenuItem value={"lunge"}>Lunge</MenuItem>
-          <MenuItem value={"free-lunge"}>Free Lunge</MenuItem>
-          <MenuItem value={"lesson"}>Lesson</MenuItem>
-          <MenuItem value={"ride"}>Ride</MenuItem>
-          <MenuItem value={"play"}>Structured Play</MenuItem>
-          <MenuItem value={"handwalk"}>Handwalk</MenuItem>
-        </Select>
-        </FormControl>  
-      </Grid>
-      
-      <Grid item xs className={classes.textfield}>
-  
-      <TextField id="filled-multiline-static" label="Please detail the exercises you worked on" variant="outlined" name="answer3" style={stylesTitle} type="text" rows={5}
-      InputLabelProps={{
-        shrink: true,
-      }}/>
- 
-      </Grid>
-      
-      <Grid item xs className={classes.textfield}>
-      {/* <StyledInputLabel htmlFor="answer4">Any other comment you feel are relevant?</StyledInputLabel> */}
-      <TextField id="filled-multiline-static" label="Any other comment you feel are relevant?" variant="outlined" name="answer4" style={stylesTextArea} type="text" rows={5}
-      InputLabelProps={{
-        shrink: true,
-      }}/>
-     
-      </Grid>
+        <Grid item xs className={classes.textfield}>
+          <FormControlLabel 
+            control = {
+              <Checkbox
+                checked={state.answer1}
+                onChange={handleChecked}
+                name="answer1"
+                color="secondary" 
+              />
+            }
+            label="Did you warm-up with a 10 minutes walk on a loose rein?"
+          />
+        </Grid>
 
-      <Grid item xs className={classes.textfield}>
-      <Button variant="contained" color="secondary" type="submit">SUBMIT</Button>
-      </Grid>
+        <Grid item xs className={classes.textfield}>
+        <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Select an activity type:</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={open}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            value={state.answer2}
+            onChange={handleSelect}
+          >
+            <MenuItem value=""> </MenuItem>
+            <MenuItem value={"lunge"}>Lunge</MenuItem>
+            <MenuItem value={"free-lunge"}>Free Lunge</MenuItem>
+            <MenuItem value={"lesson"}>Lesson</MenuItem>
+            <MenuItem value={"ride"}>Ride</MenuItem>
+            <MenuItem value={"play"}>Structured Play</MenuItem>
+            <MenuItem value={"handwalk"}>Handwalk</MenuItem>
+          </Select>
+          </FormControl>  
+        </Grid>
+        
+        <Grid item xs className={classes.textfield}>
+          <TextField 
+            id="filled-multiline-static"
+            label="Please detail the exercises you worked on"
+            variant="outlined"
+            name="answer3" onChange={handleChange}
+            value={state.answer3} 
+            style={stylesTitle} 
+            type="text" 
+            rows={5}
+            InputLabelProps={{shrink: true}}
+          /> 
+        </Grid>
+        
+        <Grid item xs className={classes.textfield}>
+          <TextField 
+            id="filled-multiline-static" 
+            label="Any other comment you feel are relevant?"
+            variant="outlined"
+            name="answer4"
+            style={stylesTextArea}
+            type="text"
+            rows={5}
+            onChange={handleChange}
+            value={state.answer4}
+            InputLabelProps={{shrink: true,}}
+          />     
+        </Grid>
+
+        <Grid item xs className={classes.textfield}>
+        <Button variant="contained" color="secondary" type="submit" onSubmit={handleSubmit}>SUBMIT</Button>
+        </Grid>
+        
+      </form>
       
-    </form>
-    
-  </Grid>
- 
-  )};
+    </Grid>
+
+  </div>
+    )};
