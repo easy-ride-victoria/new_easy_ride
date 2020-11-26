839b3e6d411247dd08e6ee37d5dcf9b38535a6fe
Iyris <iyrisvigil@gmail.com>
2020-11-26 01:23:08 -0800

    add fragment to fix error

--- app/javascript/components/Calendar/EditForm.js
+++ app/javascript/components/Calendar/EditForm.js
@@ -28,7 +28,7 @@ const EditForm = (props) => {
   const styles = useStyles();
   const { errors } = props;
   const { slotInfo, setSlotInfo } = props;
-
+  console.log("slotInfo", slotInfo);
   const [users, setUsers] = useState([]);
   useEffect(() => {
     axios.get("/api/v1/users").then((response) => {
@@ -83,120 +83,124 @@ const EditForm = (props) => {
       </DialogTitle>
       <DialogContent>
         <MuiPickersUtilsProvider utils={MomentUtils}>
-          <FormControl className={styles.formControl}>
-            <InputLabel id="booking-type-label">Booking Type</InputLabel>
-            <Select
-              readOnly
-              disabled
-              labelId="booking-type-label"
-              name="event_type"
-              value={slotInfo.event_type}
-              onChange={handleBookingChange}
-            >
-              <MenuItem value={"lesson"}>Lesson</MenuItem>
-              <MenuItem value={"ride"}>Ride</MenuItem>
-              <MenuItem value={"other"}>Other Arena Booking</MenuItem>
-            </Select>
-          </FormControl>
-          <div className={styles.dateTimePickerContainer}>
-            <DateTimePicker
-              label="Start Time"
-              name="start_time"
-              inputVariant="outlined"
-              className={styles.dateTimePicker}
-              autoOk
-              openTo="hours"
-              value={slotInfo.start_time}
-              onChange={handleStartTimeChange}
-            />
-            <DateTimePicker
-              label="End Time"
-              name="end_time"
-              inputVariant="outlined"
-              className={styles.dateTimePicker}
-              autoOk
-              openTo="hours"
-              value={slotInfo.end_time}
-              onChange={handleEndTimeChange}
-            />
-          </div>
-          {slotInfo.event_type === "lesson" && (
-            <>
-              <TextField
-                margin="dense"
-                name="lesson_price_cad"
-                label="Lesson Price"
-                type="number"
+          <>
+            <FormControl className={styles.formControl}>
+              <InputLabel id="booking-type-label">Booking Type</InputLabel>
+              <Select
+                readOnly
+                disabled
+                labelId="booking-type-label"
+                name="event_type"
+                value={slotInfo.event_type}
                 onChange={handleBookingChange}
-                value={slotInfo.lesson_price_cad}
-                fullWidth
+              >
+                <MenuItem value={"lesson"}>Lesson</MenuItem>
+                <MenuItem value={"ride"}>Ride</MenuItem>
+                <MenuItem value={"other"}>Other Arena Booking</MenuItem>
+              </Select>
+            </FormControl>
+            <div className={styles.dateTimePickerContainer}>
+              <DateTimePicker
+                label="Start Time"
+                name="start_time"
+                inputVariant="outlined"
+                className={styles.dateTimePicker}
+                autoOk
+                openTo="hours"
+                value={slotInfo.start_time}
+                onChange={handleStartTimeChange}
               />
-              <TextField
-                margin="dense"
-                name="lesson_total_spots"
-                label="Lesson Spots"
-                type="number"
-                onChange={handleBookingChange}
-                value={slotInfo.lesson_total_spots}
-                fullWidth
+              <DateTimePicker
+                label="End Time"
+                name="end_time"
+                inputVariant="outlined"
+                className={styles.dateTimePicker}
+                autoOk
+                openTo="hours"
+                value={slotInfo.end_time}
+                onChange={handleEndTimeChange}
               />
-              <List>
-                {slotInfo.rides.map((ride) => {
-                  const user = users.find((u) => Number(u.id) === ride.user_id);
-                  const horse = horses.find(
-                    (h) => Number(h.id) === ride.horse_id
-                  );
-                  console.log(ride, user, horse, users, horses);
-                  if (!user || !horse) return null;
-                  const { first_name, last_name } = user.attributes;
-                  const { name, profile_picture } = horse.attributes;
-                  return (
-                    <ListItem key={ride.id}>
-                      <ListItemAvatar>
-                        <Avatar src={profile_picture} />
-                      </ListItemAvatar>
-                      <ListItemText
-                        primary={`${first_name} ${last_name}`}
-                        secondary={name}
-                      />
-                    </ListItem>
-                  );
-                })}
-              </List>
-            </>
-          )}
-          {slotInfo.event_type === "ride" && (
-            <>
-              <FormControl className={styles.formControl}>
-                <InputLabel id="rider-select-label">Rider</InputLabel>
-                <Select
-                  labelId="rider-select-label"
-                  id="rider-select"
-                  value={rideData.user_id}
-                  onChange={(e) =>
-                    setRideData({
-                      ...rideData,
-                      user_id: Number(e.target.value),
-                    })
-                  }
-                >
-                  {users.map((user) => {
+            </div>
+            {slotInfo.event_type === "lesson" && (
+              <>
+                <TextField
+                  margin="dense"
+                  name="lesson_price_cad"
+                  label="Lesson Price"
+                  type="number"
+                  onChange={handleBookingChange}
+                  value={slotInfo.lesson_price_cad}
+                  fullWidth
+                />
+                <TextField
+                  margin="dense"
+                  name="lesson_total_spots"
+                  label="Lesson Spots"
+                  type="number"
+                  onChange={handleBookingChange}
+                  value={slotInfo.lesson_total_spots}
+                  fullWidth
+                />
+                <List>
+                  {slotInfo.rides.map((ride) => {
+                    const user = users.find(
+                      (u) => Number(u.id) === ride.user_id
+                    );
+                    const horse = horses.find(
+                      (h) => Number(h.id) === ride.horse_id
+                    );
+                    console.log(ride, user, horse, users, horses);
+                    if (!user || !horse) return null;
+                    const { first_name, last_name } = user.attributes;
+                    const { name, profile_picture } = horse.attributes;
                     return (
-                      <MenuItem value={user.id} key={user.id}>
-                        {user.attributes.first_name} {user.attributes.value}
-                      </MenuItem>
+                      <ListItem key={ride.id}>
+                        <ListItemAvatar>
+                          <Avatar src={profile_picture} />
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`${first_name} ${last_name}`}
+                          secondary={name}
+                        />
+                      </ListItem>
                     );
                   })}
-                </Select>
-              </FormControl>
-              <HorseSelect
-                rideData={rideData}
-                setRideData={setRideData}
-                errors={errors}
-                horses={horses}
-              />
-            </>
-          )}
+                </List>
+              </>
+            )}
+            {slotInfo.event_type === "ride" && (
+              <>
+                <FormControl className={styles.formControl}>
+                  <InputLabel id="rider-select-label">Rider</InputLabel>
+                  <Select
+                    labelId="rider-select-label"
+                    id="rider-select"
+                    value={rideData.user_id}
+                    onChange={(e) =>
+                      setRideData({
+                        ...rideData,
+                        user_id: Number(e.target.value),
+                      })
+                    }
+                  >
+                    {users.map((user) => {
+                      return (
+                        <MenuItem value={user.id} key={user.id}>
+                          {user.attributes.first_name} {user.attributes.value}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+                <HorseSelect
+                  rideData={rideData}
+                  setRideData={setRideData}
+                  errors={errors}
+                  horses={horses}
+                />
+              </>
+            )}
+          </>
         </MuiPickersUtilsProvider>
       </DialogContent>
       <DialogActions>
