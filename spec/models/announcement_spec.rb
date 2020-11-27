01603579c5ad115ebbff4779ec8187012bbe1097
Iyris <iyrisvigil@gmail.com>
2020-11-27 15:00:28 -0800

    add rspec tests for announcements

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should fail if start date is later than end date" do
+  @announcement = Announcement.new(title: "New announcement", start_date: DateTime.new(2020, 11, 30), end_date: DateTime.new(2020, 11, 29))
+    expect {@announcement.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it "should fail if there is no title" do
+  @announcement = Announcement.new(title: nil, start_date: DateTime.new(2020, 11, 30), end_date: DateTime.new(2020, 11, 30))
+    expect {@announcement.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it "should fail if there is no start date" do
+  @announcement = Announcement.new(title: "New announcement", start_date: nil, end_date: DateTime.new(2020, 11, 30))
+    expect {@announcement.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it "should fail if there is no end date" do
+  @announcement = Announcement.new(title: "New announcement", start_date: DateTime.new(2020, 11, 30), end_date: nil)
+    expect {@announcement.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it "should save as a new announcement" do
+    @announcement = Announcement.create!(title: "New announcement", start_date: DateTime.new(2020, 11, 30), end_date: DateTime.new(2020, 11, 30))
+  expect(@announcement.id).to be_present
+  end
 end
