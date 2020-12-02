239d0a8cd29ae4687a39fbd9e825e2e32e963a8c
philipd <philip.hd@protonmail.com>
2020-12-02 10:49:18 -0700

    Create a migration to remove the no-longer-needed password_digest column from the Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_065138) do
+ActiveRecord::Schema.define(version: 2020_12_02_173901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_065138) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "email", null: false
-    t.string "password_digest"
     t.string "hcbc_number"
     t.boolean "is_admin", default: false, null: false
     t.boolean "active", default: true, null: false
