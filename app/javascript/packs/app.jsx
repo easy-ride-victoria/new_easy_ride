9abae91230eb065821b5f59a4525e474779b8755
Nicole Woodcock <nicole.xox@gmail.com>
2020-12-19 14:09:44 -0800

    separate theme into own file

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -6,44 +6,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../components/App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#004578",
-    },
-    secondary: {
-      main: "#a47638",
-    },
-    // Used by `getContrastText()` to maximize the contrast between
-    // the background and the text.
-    contrastThreshold: 3,
-    // Used by the functions below to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
-    tonalOffset: 0.2,
-  },
-  typography: {
-    fontSize: 21,
-  },
-  overrides: {
-    MuiSelect: {
-      root: {
-        display: "flex",
-        alignItems: "center",
-      },
-    },
-    MuiOutlinedInput: {
-      multiline: {
-        // fontWeight: "bold",
-        // fontSize: "20px",
-        // color: "purple",
-        width: "30vw",
-      },
-    },
-  },
-});
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../theme";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -1,44 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import Home from "../components/Home/Home";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#004578",
-    },
-    secondary: {
-      main: "#a47638",
-    },
-    // Used by `getContrastText()` to maximize the contrast between
-    // the background and the text.
-    contrastThreshold: 3,
-    // Used by the functions below to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
-    tonalOffset: 0.2,
-  },
-  typography: {
-    fontSize: 21,
-  },
-  overrides: {
-    MuiSelect: {
-      root: {
-        display: "flex",
-        alignItems: "center",
-      },
-    },
-    MuiOutlinedInput: {
-      multiline: {
-        // fontWeight: "bold",
-        // fontSize: "20px",
-        // color: "purple",
-        width: "30vw",
-      },
-    },
-  },
-});
+import theme from "../theme";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
